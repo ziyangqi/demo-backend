91033a5f40a925abe8a9b20cc7bf5708dea131e0
zyq <ziyang_qi@163.com>
2024-08-16 16:55:35 +0800

    后端模板

---
+++ src/main/java/com/yupi/project/MyApplication.java
@@ -0,0 +1,38 @@
+package com.yupi.project;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("com.yupi.project.mapper")
+public class MyApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(MyApplication.class, args);
+
+        Environment environment = application.getEnvironment();
+        // 获取本地Ip
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        // 获取本地端口
+        String port = environment.getProperty("server.port");
+        // 获取本地路径
+        String property = environment.getProperty("server.servlet.context-path");
+        String path = StringUtils.isBlank(property) ? "" : property;
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "Swagger接口文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
+
+
+    }
+}
+
